efa1b8516a7bc765a445915241f1befed88f4c80
Chen Dong <chendongcse@gmail.com>
2022-03-25 02:41:57 +0800

    1.3 可以编译了

---
+++ src/main/java/com/tdd/args/Args.java
@@ -0,0 +1,8 @@
+package com.tdd.args;
+
+public class Args {
+    public static <T> T parse(
+    Class<T> optionsClass, String... args) {
+        return null;
+    }
+}
---
+++ src/main/java/com/tdd/args/Option.java
@@ -0,0 +1,13 @@
+package com.tdd.args;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER})
+
+public @interface Option {
+    String value();
+}
