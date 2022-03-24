c53321ed1becbc17edf043c397d64a6badaec03f
Chen Dong <chendongcse@gmail.com>
2022-03-25 02:35:03 +0800

    1.1 API 与实现方式有了方向之后，我们就可以根据需求的描述对功能进行分解了

--- src/test/java/com/tdd/args/argsTest.java
+++ src/test/java/com/tdd/args/argsTest.java
@@ -1,20 +1,36 @@
 package com.tdd.args;
 
+import com.sun.source.tree.AssertTree;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class argsTest {
     // -l -p 8080 -d /usr/logs
+    // TODO
+    //Single Option:
+    //    -Bool
+    //    -Int -p 8080
+    //    -String -d /usr/logs
+    //Multi Options: -l -p 8080 -d /usr/logs
+    //sad path:
+    // -Bool -l t / -l t f
+    // -Int -p / -p 8080 8081
+    // -String -d /  -d /usr/logs /usr/vars
+    // default value
+    // -Bool : false
+    // -Int :0
+    // -String: ""
+
+
+
     @Test
-    public void should() {
-        Arguments args = Args.parse("l:b,p:d,d:s", "-l", "-p", "8080", "-d", "/usr/logs");
-        args.getBool("l");
-        args.getInt("p");
-
+    public void should_example_1(){
         Options options = Args.parse(Options.class, "-l", "-p", "8080", "-d", "/usr/logs");
-        options.logging();
-        options.port();
-        options.directory();
-
+        assertTrue(options.logging());
+        assertEquals(8080,options.port());
+        assertEquals("/usr/logs",options.directory());
     }
 
     static record Options(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
