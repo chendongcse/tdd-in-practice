c3e9ab5d2c73a01ef9d655dc9523fff3f610d5f8
Chen Dong <chendongcse@gmail.com>
2022-03-26 01:38:24 +0800

    1.5 测试通过了

--- src/main/java/com/tdd/args/Args.java
+++ src/main/java/com/tdd/args/Args.java
@@ -1,8 +1,14 @@
 package com.tdd.args;
 
+import java.lang.reflect.Constructor;
+
 public class Args {
-    public static <T> T parse(
-    Class<T> optionsClass, String... args) {
-        return null;
+    public static <T> T parse(Class<T> optionsClass, String... args){
+        Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
+        try {
+            return (T) constructor.newInstance(true);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
     }
 }
