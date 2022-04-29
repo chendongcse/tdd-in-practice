88d1f0ad596f7688ec1341de6907bfd12a10ed46
Chen Dong <chendongcse@gmail.com>
2022-04-30 02:46:45 +0800

    4.1 fix bad smell : 意图不够直观

--- src/main/java/com/tdd/args/BooleanParser.java
+++ src/main/java/com/tdd/args/BooleanParser.java
@@ -2,11 +2,14 @@ package com.tdd.args;
 
 import java.util.List;
 
+import static com.tdd.args.SingleValuedParser.values;
+
 class BooleanParser implements OptionParser<Boolean> {
     @Override
     public Boolean parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-"+ option.value());
-        if(index + 1 < arguments.size() && !arguments.get(index+1).startsWith("-")){
+        List<String> values = values(arguments, index);
+        if(values.size() > 0 ){
             throw new TooManyArgumentsException(option.value());
         }
         return arguments.contains("-" + option.value());
--- src/main/java/com/tdd/args/SingleValuedParser.java
+++ src/main/java/com/tdd/args/SingleValuedParser.java
@@ -1,7 +1,9 @@
 package com.tdd.args;
 
 import java.util.List;
+import java.util.OptionalInt;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 public class SingleValuedParser<T> implements OptionParser<T> {
     Function<String, T> valueParser;
@@ -19,14 +21,21 @@ public class SingleValuedParser<T> implements OptionParser<T> {
         if(index == -1){
             return defaultValue;
         }
-        if(index+1 == arguments.size() || ( index+1 < arguments.size() &&  arguments.get(index+1).startsWith("-"))){
+        List<String> values = values(arguments, index);
+        if(values.size() < 1 ){
             throw new InsufficientArgumentsException(option.value());
         }
-        if(index+2 < arguments.size() && !arguments.get(index+2).startsWith("-")){
+        if(values.size() > 1 ){
             throw new TooManyArgumentsException(option.value());
         }
-        String value = arguments.get(index + 1);
+        String value = values.get(0);
         return valueParser.apply(value);
+    }
+
+    static List<String> values(List<String> arguments, int index) {
+        int followingFlag = IntStream.range(index + 1, arguments.size()).filter(it -> arguments.get(it).startsWith("-")).findFirst().orElse(arguments.size());
+        List<String> values = arguments.subList(index + 1, followingFlag);
+        return values;
     }
 
 }
