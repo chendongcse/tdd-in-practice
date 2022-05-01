db28306b38b1006aaa9e4740a9642515c7058bd6
Chen Dong <chendongcse@gmail.com>
2022-05-01 19:03:03 +0800

    4.5 r:should_parse_list_value

--- src/test/java/com/tdd/args/argsTest.java
+++ src/test/java/com/tdd/args/argsTest.java
@@ -1,6 +1,5 @@
 package com.tdd.args;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,13 +19,23 @@ public class argsTest {
     }
 
     @Test
-    public void should_throw_illegal_exception_if_annotation_not_present(){
+    public void should_throw_illegal_exception_if_annotation_not_present() {
         IllegalOptionException e = assertThrows(IllegalOptionException.class, () -> Args.parse(OptionWithoutAnnotation.class, "-l", "-p", "8080", "-d", "/usr/logs"));
         assertEquals("port", e.getParamter());
     }
 
-    static record OptionWithoutAnnotation(@Option("l") boolean logging,int port, @Option("d") String directory) {
+    static record OptionWithoutAnnotation(@Option("l") boolean logging, int port, @Option("d") String directory) {
 
+    }
+
+    @Test
+    public void should_parse_list_value() {
+        ListOption options = Args.parse(ListOption.class, "-g", "this", "is", "a", "list", "-d", "1", "2", "-3", "4");
+        assertArrayEquals(new String[]{"this", "is", "a", "list"}, options.group);
+        assertArrayEquals(new Integer[]{1, 2, -3, 4}, options.decimals);
+    }
+
+    static record ListOption(@Option("g") String[] group, @Option("d") Integer[] decimals) {
     }
 
 }
