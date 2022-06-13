f301a45c9d3bc08434d4fa94d8f22b7fe35b2536
Chen Dong <chendongcse@gmail.com>
2022-06-14 00:41:00 +0800

    13.1 g:should_bind_type_to_a_specific_instance

--- dependency-injection-container/src/main/java/com/tdd/di/Context.java
+++ dependency-injection-container/src/main/java/com/tdd/di/Context.java
@@ -1,10 +1,17 @@
 package com.tdd.di;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Context {
+    Map<Class<?>, Object> components = new HashMap<>();
+
     public <componentType> void bind(Class<componentType> type, componentType instance) {
+        components.put(type, instance);
     }
 
     public <componentType> componentType get(Class<componentType> typeClass) {
-        return null;
+        return (componentType) components.get(typeClass);
     }
 }
+
