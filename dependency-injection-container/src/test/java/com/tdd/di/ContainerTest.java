f518c6a1be87f8255e42c619e35306c978802c19
Chen Dong <chendongcse@gmail.com>
2022-06-14 00:25:53 +0800

    13.1 list some todos and let's get start!

---
+++ dependency-injection-container/src/test/java/com/tdd/di/ContainerTest.java
@@ -0,0 +1,39 @@
+package com.tdd.di;
+
+import org.junit.jupiter.api.Nested;
+
+public class ContainerTest {
+
+    @Nested
+    public class ConpomentConstruction {
+        //todo: instance
+        //todo: interface
+        //todo: abstract class
+        @Nested
+        public class ConstructorInjection {
+
+        }
+
+        @Nested
+        public class FieldInjection {
+
+        }
+
+        @Nested
+        public class MethodInjection{
+
+        }
+
+    }
+
+    @Nested
+    public class DependenciesSelection {
+
+    }
+
+    @Nested
+    public class LifecycleManagement {
+
+    }
+
+}
