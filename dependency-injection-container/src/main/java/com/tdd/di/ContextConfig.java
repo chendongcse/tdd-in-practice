f7b75ded851b67640eae5b83d0767fa1cbad2bd1
Chen Dong <chendongcse@gmail.com>
2022-06-20 01:02:25 +0800

    17.1 provider 只在bind()用到了，get()时候没用到，可以删掉了，至此我们完成了对provider的替换。

--- dependency-injection-container/src/main/java/com/tdd/di/ContextConfig.java
+++ dependency-injection-container/src/main/java/com/tdd/di/ContextConfig.java
@@ -14,19 +14,16 @@ import java.util.stream.Collectors;
 import static java.util.Arrays.stream;
 
 public class ContextConfig{
-    private Map<Class<?>, Provider<?>> providers = new HashMap<>();
-    private Map<Class<?>, ComponentProvider<?>> componentProviders = new HashMap<>();
+    private Map<Class<?>, ComponentProvider<?>> providers = new HashMap<>();
 
     public <Type> void bind(Class<Type> type, Type instance) {
-        providers.put(type, (Provider<Type>) () -> instance);
-        componentProviders.put(type, context -> instance);
+        providers.put(type, context -> instance);
     }
 
     public <Type, Implementation extends Type>
     void bind(Class<Type> type, Class<Implementation> implementation) {
         Constructor<Implementation> injectConstructor = getInjectConstructor(implementation);
         providers.put(type, new ConstructorInjectionProvider(type, injectConstructor));
-        componentProviders.put(type, new ConstructorInjectionProvider(type, injectConstructor));
     }
 
     public Context getContext(){
@@ -34,7 +31,7 @@ public class ContextConfig{
         return new Context() {
             @Override
             public <Type> Optional<Type> get(Class<Type> type) {
-                return Optional.ofNullable(componentProviders.get(type)).map(provider -> (Type) provider.get(this));
+                return Optional.ofNullable(providers.get(type)).map(provider -> (Type) provider.get(this));
             }
         };
     }
@@ -43,7 +40,7 @@ public class ContextConfig{
         T get(Context context);
     }
 
-    class ConstructorInjectionProvider<T> implements Provider<T>, ComponentProvider<T> {
+    class ConstructorInjectionProvider<T> implements ComponentProvider<T> {
         private Class<?> componentType;
         private Constructor<T> injectConstructor;
         private boolean constructing = false;
@@ -51,29 +48,6 @@ public class ContextConfig{
         public ConstructorInjectionProvider(Class<?> componentType, Constructor<T> injectConstructor) {
             this.componentType = componentType;
             this.injectConstructor = injectConstructor;
-        }
-
-        @Override
-        public T get() {
-            return getT(getContext());
-        }
-
-        private T getT(Context context) {
-            if (constructing) throw new CyclicDependenciesFoundException(componentType);
-            try {
-                constructing = true;
-                Object[] dependencies = stream(injectConstructor.getParameters())
-                        .map(p -> context.get(p.getType())
-                                .orElseThrow(() -> new DependencyNotFoundException(componentType,p.getType())))
-                        .toArray(Object[]::new);
-                return injectConstructor.newInstance(dependencies);
-            } catch (CyclicDependenciesFoundException e) {
-                throw new CyclicDependenciesFoundException(componentType, e);
-            } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
-                throw new RuntimeException(e);
-            } finally {
-                constructing = false;
-            }
         }
 
         @Override
